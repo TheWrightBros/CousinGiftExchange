d45e163ce3c20b9a71a42b8bca8035f813eb861f
Dylan Wright <dylansamuelwright@gmail.com>
2016-02-27 14:31:46 -0800

    added react

---
+++ app/assets/javascripts/app/test/test.jsx
@@ -0,0 +1,22 @@
+/*
+
+  Main Mobile Auto Quote React Application
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+*/
+
+"use strict";
+// Import React
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var App = React.createClass({
+  render: function(){
+
+    return (
+      <div> a simple react div</div>
+    );
+  }
+});
+
+ReactDOM.render(<App />, document.getElementById('replace'));
